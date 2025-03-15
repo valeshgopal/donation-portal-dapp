26d2efe8ce5fed0bcb87f5292b2db1c484a2319e
Valesh Gopal <valesh.dev@gmail.com>
2025-03-16 05:16:49 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     output: 'standalone',
     reactStrictMode: true,
     images: {
-        remotePatterns: ['ipfs.io', 'images.unsplash.com'], // Allow IPFS gateway for images and Unsplash
+        domains: ['ipfs.io', 'images.unsplash.com'], // Allow IPFS gateway for images and Unsplash
     },
     webpack: (config) => {
         config.resolve.fallback = {
