8df9933b57fd702c829a5210422c435178555be4
Valesh Gopal <valesh.dev@gmail.com>
2025-03-16 05:53:23 +0530

    update nexy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'standalone',
     reactStrictMode: true,
     images: {
         domains: ['ipfs.io', 'images.unsplash.com'], // Allow IPFS gateway for images and Unsplash
